4b32f86ec15953ae341a34df07bb53e6895c7b2b
Daniel Rempel <danil.rempel@gmail.com>
2015-05-30 21:52:43 +0300

    fix memory map definition

--- sysroot/usr/src/kernel/kernel/mb-memory-map.h
+++ sysroot/usr/src/kernel/kernel/mb-memory-map.h
@@ -1,10 +1,10 @@
 
 typedef struct multiboot_memory_map {
 	unsigned int size;
-	unsigned long base_addr;
+	uint64_t base_addr;
 // You can also use: unsigned long long int base_addr; if supported.
 //	unsigned int length_low,length_high;
-	unsigned long length;
+	uint64_t length;
 // You can also use: unsigned long long int length; if supported.
 	unsigned int type;
 } multiboot_memory_map_t;
